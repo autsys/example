69c73585ab6859129dddb68f04ceb1adee2f9ce7
seanbass <seandbass@gmail.com>
2019-11-12 15:18:13 -0800

    added jumbo and logo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 //import { YesButt, NoButt, SkipButt, ButtonG } from './components/button';
 import QCard from './components/card';
-import Logo from './components/logo'
+import Logo from './components/logo';
+import Jumbo from './components/jumbo';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
+        <Jumbo />
         <Logo />
         <QCard style={{position: 'absolute'}}/>
       </Fragment>
--- src/components/logo.js
+++ src/components/logo.js
@@ -5,7 +5,11 @@ import Image from 'react-bootstrap/Image'
 export default class Logo extends Component {
   render() {
     return(
-          <Image src="../image/AS_Logo.png" fluid />
+          <Image 
+            //style={{position: 'absolute'}}
+            src="../image/AS_Logo.png" 
+            fluid 
+          />
     )
   }
 }
