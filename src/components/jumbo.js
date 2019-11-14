b57a997e2028ec3bc0f3a89891f4e1fcac32cfbd
seanbass <seandbass@gmail.com>
2019-11-14 14:22:03 -0800

    jumbo style

--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -5,14 +5,14 @@ import Button from 'react-bootstrap/Button';
 export default class Jumbo extends Component {
   render() {
     return(
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '5%'}}>
+      <div style={{display: 'flex', alignItems: 'center', marginTop: '5%'}}>
         <Jumbotron style={{color: 'white', backgroundColor: '#333333'}}>
           <h1>Welcome!</h1>
           <p>
             This is a simple questionare to help us, help you! It should take no longer than 2 minutes.
           </p>
           <p>
-            <Button style={{width: 100}} variant="primary">Start</Button>
+            <Button style={{width: 120}} variant="primary">Start</Button>
           </p>
         </Jumbotron>
       </div>
