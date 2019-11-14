5ad20ea59266c9f6a39fc7cfca9f1df5846d8c23
seanbass <seandbass@gmail.com>
2019-11-13 16:07:14 -0800

    changed logo(not sure which I like better) - fixed up jumbo

--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
 
 export default class Jumbo extends Component {
   render() {
     return(
-      <Jumbotron style={{textAlign: 'center', backgroundColor:'white', alignSelf: 'center', marginTop: '5%'}}>
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '5%'}}>
+        <Jumbotron style={{color: 'white', backgroundColor: '#333333'}}>
+          <h1>Welcome!</h1>
           <p>
-            Thanks for taking time to help us, help you!
+            This is a simple questionare to help us, help you! It should take no longer than 2 minutes.
           </p>
           <p>
-            Click HERE to get started.
+            <Button style={{width: 100}} variant="primary">Start</Button>
           </p>
-      </Jumbotron>
+        </Jumbotron>
+      </div>
     )
   }
 }
--- src/components/logo.js
+++ src/components/logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Logo() {
   return(
     <img 
-      style={{width: 100, height: 100, marginLeft: '2%', marginTop: '2%'}} 
+      style={{width: 100, height: 100, marginLeft: '2%', marginTop: '1%'}} 
       src={require('../image/AS_Logo.png')}
       alt="description of image"
     />
