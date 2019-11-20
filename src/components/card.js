ef11f41ba7bfdd2bca0577bc3bc7df9537f72991
seanbass <seandbass@gmail.com>
2019-11-20 03:11:17 -0800

    prep to start json questions brought into Qcard

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card'
-import globalHook from 'use-global-hook';
-import questions from '../questions.json'
 import { Button, ButtonGroup } from 'react-bootstrap';
 
-const initialState = {
-  counter: 0,
-};
- 
-const actions = {
-  addToCounter: (store, amount) => {
-    const newCounterValue = store.state.counter + amount;
-    store.setState({ counter: newCounterValue });
-  },
-};
- 
-const useGlobal = globalHook(React, initialState, actions);
- 
+import questions from '../questions.json'
+
 const Qcard = () => {
 
-    const [globalState, globalActions] = useGlobal();
+    const [questions, setQuestions] = useState();
 
     return (
       <Card style={{marginLeft: '20%', marginRight: '20%', marginTop: '10%'}}>
@@ -28,9 +15,9 @@ const Qcard = () => {
             <Card.Title>{Object.values(questions)[0].Question}</Card.Title>
               <div className="d-flex flex-column">
                 <ButtonGroup size="lg">
-                  <Button type="button" onClick={() => globalActions.addToCounter(1)}>Yes</Button>
-                  <Button onClick={() => globalActions.addToCounter(1)}>No</Button>
-                  <Button onClick={() => globalActions.addToCounter(1)}>Skip</Button>
+                  <Button onClick={() => setQuestions()}>Yes</Button>
+                  <Button onClick={() => setQuestions()}>No</Button>
+                  <Button onClick={() => setQuestions()}>Skip</Button>
                 </ButtonGroup>
               </div>
           </Card.Body>
