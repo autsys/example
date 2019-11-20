d7fb0d812508ac548cd0cba03c1b134c70363e03
seanbass <seandbass@gmail.com>
2019-11-20 03:17:26 -0800

    space

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card'
 import { Button, ButtonGroup } from 'react-bootstrap';
-
 import questions from '../questions.json'
 
 const Qcard = () => {
