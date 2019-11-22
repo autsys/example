802cef1abbb553a0879cb784792ef52003134ade
seanbass <seandbass@gmail.com>
2019-11-22 04:55:37 -0800

    question title now counts with questions

--- src/components/card.js
+++ src/components/card.js
@@ -10,25 +10,23 @@ const Qcard = () => {
 
     const [steps, setSteps] = useState(0);
 
+
       function increment() {
         setSteps(steps => steps + 1);
-        if (steps >= 12) {
-          return <Thanks />;
-        }
       }
 
       // breaks after you reach the end need to set a limit
 
     return (
       <Card style={{marginLeft: '20%', marginRight: '20%', marginTop: '10%'}}>
-        <Card.Header>Question #1</Card.Header>
+        <Card.Header>Question {steps + 1}</Card.Header>
           <Card.Body>
             <Card.Title>{Object.values(questions)[steps].Question}</Card.Title>
               <div className="d-flex flex-column">
                 <ButtonGroup size="lg">
-                  <Button onClick={increment}>Yes</Button>
-                  <Button onClick={increment}>No</Button>
-                  <Button onClick={increment}>Skip</Button>
+                  <Button type='Yes' onClick={increment}>Yes</Button>
+                  <Button type='No' onClick={increment}>No</Button>
+                  <Button type='Skip' onClick={increment}>Skip</Button>
                 </ButtonGroup>
               </div>
           </Card.Body>
