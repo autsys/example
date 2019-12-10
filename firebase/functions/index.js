49efe1241fd9ac5137fdd73e6cf267708731f07b
Jeremy Heyer <jheyer159@gmail.com>
2019-12-09 19:40:52 -0500

    Working firestore submission / retrieval

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,11 +1,48 @@
 const cors = require('cors')
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-exports.submit = functions.https.onRequest((request, response) => {
-  const json = request.body
-  const now = new Date().toUTCString()
-  console.log(now, JSON.stringify(json, null, 2))
-  cors()(request, response, () => {
-    response.send("Thank you for the submission.");
-  });
+admin.initializeApp(functions.config().firebase);
+let db = admin.firestore();
+
+exports.submit = functions.https.onRequest( async (request, response) => {
+  try {
+    const json = request.body
+    const now = new Date().toUTCString()
+    console.log(now, JSON.stringify(json, null, 2))
+    cors()(request, response, () => {
+      response.send("Thank you for the submission.");
+    });
+    let docRef = db.collection('users').doc('alovelace');
+
+    let setAda = docRef.set({
+      first: 'Ada',
+      last: 'Lovelace',
+      born: 1815
+    });
+  }
+  catch(err){
+    console.error(err)
+    response.send('Error with request')
+  }
 });
+
+exports.data = functions.https.onRequest( async (request, response) => {
+  try {
+    let collection = await db.collection('users')
+    const snapshot = await collection.get()
+    console.log('snapshot', JSON.stringify(snapshot))
+    let result = []
+    snapshot.forEach( doc => {
+      const d = doc.data()
+      console.log('d', d)
+      result.push(d)
+    })
+    console.log('result', result)
+    response.send(result)
+  }
+  catch(err){
+    console.error(err)
+    response.send('Error with request')
+  }
+})
---
+++ firebase/functions/test2.sh
@@ -0,0 +1,2 @@
+URL=localhost:5000/autarky-systems/us-central1/data
+curl -X GET -H "Content-Type:application/json" $URL
